250fb6b93b13f4815232f16cde2073e081e005aa
Hafizah Muhyideen <40233874+justfizah@users.noreply.github.com>
2019-12-16 01:12:41 +0100

    Update validation.php

--- validation.php
+++ validation.php
@@ -6,11 +6,11 @@ header('location: index.php');
 
 
 
-$con = mysqli_connect('localhost','root','');
+$con = mysqli_connect('j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','root','');
 
 
 
-mysqli_select_db($con, 'haffyfashionluxury');
+mysqli_select_db($con, 'uqxhpq33hdq5tvnk');
 
 // if(isset($_POST['value'])){
 $username = $_POST['user'];
